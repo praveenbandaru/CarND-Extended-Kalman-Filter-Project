3a00b60bd051633fa94cbd2a53f761a92383f6a2
Praveen Bandaru <praveen.bandaru@one.verizon.com>
2018-09-30 06:23:08 -0500

    Working code files tested using Visual Studio 2017 on Windows 10.

--- src/tools.h
+++ src/tools.h
@@ -9,25 +9,35 @@ using namespace std;
 
 class Tools {
 public:
-  /**
-  * Constructor.
-  */
-  Tools();
+	/**
+	* Constructor.
+	*/
+	Tools();
 
-  /**
-  * Destructor.
-  */
-  virtual ~Tools();
+	/**
+	* Destructor.
+	*/
+	virtual ~Tools();
 
-  /**
-  * A helper method to calculate RMSE.
-  */
-  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+	/**
+	* A helper method to calculate RMSE.
+	*/
+	VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
 
-  /**
-  * A helper method to calculate Jacobians.
-  */
-  MatrixXd CalculateJacobian(const VectorXd& x_state);
+	/**
+	* A helper method to calculate Jacobians.
+	*/
+	MatrixXd CalculateJacobian(const VectorXd& x_state);
+
+	/**
+	* A helper method to calculate h.
+	*/
+	VectorXd Tools::Calculateh(const VectorXd &x_state);
+
+	/**
+	* A helper method to ConvertPolarToCartesian.
+	*/
+	VectorXd Tools::ConvertPolarToCartesian(const VectorXd &raw_measurements_);
 
 };
 
